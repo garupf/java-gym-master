05143942e02bb897086eefd8fdcf093acff2d4c8
garupf <maestro-tmn@yandex.ru>
2025-11-24 18:42:50 +0500

    добавил проверку нескольких тренировок в одно и то же время

--- test/ru/yandex/practicum/gym/TimetableTest.java
+++ test/ru/yandex/practicum/gym/TimetableTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class TimetableTest {
 
     @Test
-    void testGetTrainingSessionsForDaySingleSession(DayOfWeek dayOfWeek) {
+    void testGetTrainingSessionsForDaySingleSession() {
         Timetable timetable = new Timetable();
 
         Group group = new Group("Акробатика для детей", Age.CHILD, 60);
